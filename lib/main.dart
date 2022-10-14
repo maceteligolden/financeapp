a5aeed090b7cfab43ef8a0788063c487867eb067
maceteli <maceteligolden@gmail.com>
2022-10-14 23:29:00 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: const HomePage(),
+      theme: ThemeData(primarySwatch: Colors.purple),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Awesome App"),
+      ),
+      body: Container(
+        child: Text("Hi Flutter")
+      )
+    );
+  }
+}
