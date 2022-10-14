ffb9d372986902418610edacb665c324882a4128
maceteli <maceteligolden@gmail.com>
2022-10-14 23:54:21 +0100

    adding row and column code

--- lib/main.dart
+++ lib/main.dart
@@ -18,37 +18,37 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Awesome App"),
       ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          width: 100,
-          height: 100,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(10),
-            gradient: const LinearGradient(colors: [
-              Colors.pink,
-              Colors.yellow,
-            ]),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                blurRadius: 10, 
-              )
-            ]
-          ),
-          child: const Text(
-            " I am a box",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            )
-          ),
+      body: Container(
+        color: Colors.teal,
+        height: 500,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children:<Widget> [
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              alignment: Alignment.center,
+              color: Colors.red,
+              ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              alignment: Alignment.center,
+              color: Colors.yellow,
+              ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              alignment: Alignment.center,
+              color: Colors.green,
+              ),
+          ],
         ),
-      )
-    );
+      ),
+      );
   }
 }
